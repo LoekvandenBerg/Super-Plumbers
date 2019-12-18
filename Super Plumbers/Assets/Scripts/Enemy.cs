4f53ea063a4bba545638188981f6afcae34e33fe
Loek <L_V_D_BERG@hotmail.com>
2019-12-18 11:19:03 +0100

    Portfolio commit

---
+++ Super Plumbers/Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int points;
+    private Rigidbody2D body;
+    [SerializeField]
+    private float speed;
+    private Vector2 movementDir;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        //movementDir = Vector2.right;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move(movementDir);
+    }
+
+    public void Move(Vector2 direction)
+    {
+        movementDir = direction;
+        body.velocity = new Vector2(direction.x * speed, body.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            movementDir *= -1f;
+        }
+    }
+}
---
+++ Super Plumbers/Assets/Scripts/Game.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class Game : MonoBehaviour
+{
+    public int lives, score, highscore;
+    [SerializeField]
+    private Player player;
+    [SerializeField]
+    private Transform playerSpawnPoint;
+    [SerializeField]
+    private Spawner[] spawners;
+    private int level;
+    [SerializeField]
+    private TextMeshProUGUI scoreText, livesText, bestText;
+
+    private void Start()
+    {
+        highscore = PlayerPrefs.GetInt("HighScore", 0);
+        Load();
+        bestText.text = "Best: " + highscore;
+        UpdateHUD();
+    }
+
+    public void LoseLife()
+    {
+        if(lives > 0)
+        {
+            //respawn player
+            StartCoroutine(Respawn());
+            //deduct life
+        }
+        else
+        {
+            EndGame();
+        }
+    }
+
+    void EndGame()
+    {
+        if(score > highscore)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+        StartNewGame();
+    }
+
+    IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(2f);
+        lives--;
+        Instantiate(player.gameObject, playerSpawnPoint.position, Quaternion.identity);
+        // update hud show new life count
+    }
+
+    public void AddPoints(int amount)
+    {
+        score += amount;
+        //update hud
+        //is level complete?
+    }
+
+    void CheckLevelCompletion()
+    {
+        if(!FindObjectOfType<Enemy>())
+        {
+            foreach(Spawner spawner in spawners)
+            {
+                if (!spawner.completed)
+                {
+                    return;
+                }
+            }
+            CompleteLevel();
+        }
+    }
+
+    void CompleteLevel()
+    {
+        level++;
+        Save();
+        //load new level
+        if (SceneManager.GetSceneByBuildIndex(level) != null)
+        {
+            SceneManager.LoadScene(level);
+        }
+        else
+        {
+            Debug.Log("Game Won!");
+        }
+    }
+
+    void Save()
+    {
+        PlayerPrefs.SetInt("Score", score);
+        PlayerPrefs.SetInt("Lives", lives);
+        PlayerPrefs.SetInt("Level", level);
+    }
+
+    private void Load()
+    {
+        score = PlayerPrefs.GetInt("Score", 0);
+        lives = PlayerPrefs.GetInt("Lives", 3);
+        level = PlayerPrefs.GetInt("Level", 0);
+    }
+
+    void StartNewGame()
+    {
+        level = 0;
+        SceneManager.LoadScene(level);
+        PlayerPrefs.DeleteKey("Score"); 
+        PlayerPrefs.DeleteKey("Lives");
+        PlayerPrefs.DeleteKey("Level");
+    }
+
+    void UpdateHUD()
+    {
+        scoreText.text = "Score: " + score;
+        livesText.text = "Lives: " + lives;
+    }
+}
